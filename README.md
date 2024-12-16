2acc84ab7701e6cda59f4c1906359345490c0809
Lizb6626 <lizhibingcst@gmail.com>
2024-12-16 17:14:40 +0800

    release inference code

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# IDArb
-Official repo for "IDArb: Intrinsic Decomposition for arbitrary number of input views and illuminations"
+# IDArb: Intrinsic Decomposition for Arbitrary Number of Input Views and Illuminations
+
+<div align="center">
+
+ <a href='https://lizb6626.github.io/IDArb/'><img src='https://img.shields.io/badge/Project-Page-Green'></a> &nbsp;
+ <a href='https://huggingface.co/lizb6626/IDArb'><img src='https://img.shields.io/badge/%F0%9F%A4%97%20Hugging%20Face-Dataset-blue'></a> &nbsp;
+ <!-- <a href='https://lizb6626.github.io/IDArb/'><img src='https://img.shields.io/badge/arXiv-2412.07759-b31b1b.svg'></a> &nbsp; -->
+
+**[Zhibing Li<sup>1</sup>](https://lizb6626.github.io/), 
+[Tong Wu<sup>1 &dagger;</sup>](https://wutong16.github.io/), 
+[Jing Tan<sup>1</sup>](https://sparkstj.github.io/), 
+[Mengchen Zhang<sup>2,3</sup>](https://kszpxxzmc.github.io/), 
+[Jiaqi Wang<sup>3</sup>](https://myownskyw7.github.io/), 
+[Dahua Lin<sup>1,3 &dagger;</sup>](http://dahua.site/)** 
+<br>
+<sup>1</sup>The Chinese University of Hong Kong
+<sup>2</sup>Zhejiang University
+<sup>3</sup>Shanghai AI Laboratory
+<br>
+&dagger;: Corresponding Authors
+
+</div>
+
+- [x] Release inference code and pretrained checkpoints.
+- [ ] Release training code.
+
+## Install
+
+Our environment has been tested on CUDA 11.8 with A100.
+
+```
+git clone git@github.com:Lizb6626/IDArb.git && cd IDArb
+conda create -n idarb python==3.8 -y
+conda activate idarb
+conda install pytorch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install -r requirements.txt
+```
+
+## Inference
+
+### Single Image Intrinsic Decomposition
+```
+python main.py --data_dir example/single --output_dir output/single --input_type single
+```
+
+### Multi-view Intrinsic Decomposition
+For multi-view intrinsic decomposition, camera pose can be incorporated by enabling the `--cam` option.
+```
+## --num_views: number of input views
+
+# Without camera pose information
+python main.py --data_dir example/multi --output_dir output/multi --input_type multi --num_views 4
+
+# With camera pose information
+python main.py --data_dir example/multi --output_dir output/multi --input_type multi --num_views 4 --cam
+```
+
+## Acknowledgement
+
+This project relies on many amazing repositories. Thanks to the authors for sharing their code and data.
+
+- [Wonder3D](https://github.com/xxlong0/Wonder3D)
+- [GeoWizard](https://github.com/fuxiao0719/GeoWizard)
+- [EscherNet](https://github.com/kxhit/EscherNet)
+- [Stanford-ORB](https://github.com/StanfordORB/Stanford-ORB)
+- [NeRD](https://github.com/cgtuebingen/NeRD-Neural-Reflectance-Decomposition/tree/master)
+
+<!-- ## BibTex -->
---
+++ main.py
@@ -0,0 +1,131 @@
+import argparse
+import os
+import json
+import torch
+from PIL import Image
+import numpy as np
+from packaging import version
+
+from accelerate import Accelerator
+from diffusers.utils.import_utils import is_xformers_available
+from diffusers import AutoencoderKL, DDPMScheduler, DDIMScheduler
+from transformers import CLIPTextModel, CLIPTokenizer, CLIPImageProcessor
+
+from idarbdiffusion.models.unet_dr2d_condition import UNetDR2DConditionModel
+from idarbdiffusion.pipelines.pipeline_idarbdiffusion import IDArbDiffusionPipeline
+from idarbdiffusion.data.custom_dataset import CustomDataset
+from idarbdiffusion.data.custom_mv_dataset import CustomMVDataset
+
+
+def reform_image(img_nd):
+    albedo, normal = img_nd[0, ...], img_nd[1, ...]
+    mro = img_nd[2, ...]
+    mtl, rgh = mro[:1, ...], mro[1:2, ...]
+    mtl, rgh = np.repeat(mtl, 3, axis=0), np.repeat(rgh, 3, axis=0)
+    img_reform = np.concatenate([albedo, normal, mtl, rgh], axis=-1)
+    return img_reform.transpose(1, 2, 0)
+
+
+def save_image(out, name):
+    Nv = out.shape[0]
+    for i in range(Nv):
+        img = reform_image(out[i])
+        img = (img * 255).astype(np.uint8)
+        Image.fromarray(img).save(os.path.join(output_dir, f'{name}_{(i):02d}.png'))
+
+
+def load_pipeline():
+    """
+    load pipeline from hub
+    or load from local ckpts: pipeline = IDArbDiffusionPipeline.from_pretrained("./pipeckpts")
+    """
+    text_encoder = CLIPTextModel.from_pretrained("lizb6626/IDArb", subfolder="text_encoder")
+    tokenizer = CLIPTokenizer.from_pretrained("lizb6626/IDArb", subfolder="tokenizer")
+    feature_extractor = CLIPImageProcessor.from_pretrained("lizb6626/IDArb", subfolder="feature_extractor")
+    vae = AutoencoderKL.from_pretrained("lizb6626/IDArb", subfolder="vae")
+    scheduler = DDIMScheduler.from_pretrained("lizb6626/IDArb", subfolder="scheduler")
+    unet = UNetDR2DConditionModel.from_pretrained("lizb6626/IDArb", subfolder="unet")
+    pipeline = IDArbDiffusionPipeline(
+        text_encoder=text_encoder,
+        tokenizer=tokenizer,
+        feature_extractor=feature_extractor,
+        vae=vae,
+        unet=unet,
+        safety_checker=None,
+        scheduler=scheduler,
+    )
+    return pipeline
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_dir', type=str, default='example/single', help='Input data directory')
+parser.add_argument('--input_type', type=str, default='single', choices=['single', 'multi'], help='Specify input type (single or multi)')
+parser.add_argument('--num_views', type=int, default=4, help='Number of views')
+parser.add_argument('--cam', default=False, action='store_true', help='Enable to use camera pose')
+parser.add_argument('--output_dir', type=str, default='output', help='Output directory')
+args = parser.parse_args()
+
+
+weight_dtype = torch.float16
+
+pipeline = load_pipeline()
+
+if is_xformers_available():
+    import xformers
+    xformers_version = version.parse(xformers.__version__)
+    pipeline.unet.enable_xformers_memory_efficient_attention()
+    print(f'Use xformers version: {xformers_version}')
+
+pipeline.to("cuda")
+print("Pipeline loaded successfully")
+
+if args.input_type == 'single':
+    dataset = CustomDataset(
+        root_dir=args.data_dir,
+    )
+else:
+    dataset = CustomMVDataset(
+        root_dir=args.data_dir,
+        num_views=args.num_views,
+        use_cam=args.cam,
+    )
+
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False)
+
+output_dir = args.output_dir
+os.makedirs(output_dir, exist_ok=True)
+
+Nd = 3
+
+for i, batch in enumerate(dataloader):
+
+    imgs_in, imgs_mask, task_ids = batch['imgs_in'], batch['imgs_mask'], batch['task_ids']
+    cam_pose = batch['pose']
+    imgs_name = batch['data_name']
+
+    imgs_in = imgs_in.to(weight_dtype).to("cuda")
+    cam_pose = cam_pose.to(weight_dtype).to("cuda")
+
+    B, Nv, _, H, W = imgs_in.shape
+
+    imgs_in, imgs_mask, task_ids = imgs_in.flatten(0,1), imgs_mask.flatten(0,1), task_ids.flatten(0,2)
+
+    with torch.autocast("cuda"):
+        out = pipeline(
+                imgs_in,
+                task_ids,
+                num_views=Nv,
+                cam_pose=cam_pose,
+                height=H, width=W,
+                # generator=generator,
+                guidance_scale=1.0,
+                output_type='pt',
+                num_images_per_prompt=1,
+                eta=1.0,
+            ).images
+
+        out = out.view(B, Nv, Nd, *out.shape[1:])
+        out = out.detach().cpu().numpy()
+
+        for i in range(B):
+            save_image(out[i], imgs_name[i])
